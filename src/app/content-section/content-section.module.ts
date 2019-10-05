a442b57e96db24c0210a50cc24265f858871753f
Moritz Tiedje <moritztiedje@gmail.com>
2019-10-05 20:08:10 +0200

    Reformat and delete unused code

--- src/app/content-section/content-section.module.ts
+++ src/app/content-section/content-section.module.ts
@@ -4,7 +4,6 @@ import { ContentSectionComponent } from './content-section.component';
 import { CharacterComponent } from './character/character.component';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 import { LocationComponent } from './location/location.component';
-import { ImageTextBoxComponent } from './util/image-text-box/image-text-box.component';
 import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { MessagesComponent } from './messages/messages.component';
         CharacterComponent,
         UnderConstructionComponent,
         LocationComponent,
-        ImageTextBoxComponent,
         MessagesComponent,
     ],
     imports: [
