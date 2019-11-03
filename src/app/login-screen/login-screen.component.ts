b1dc97a4e841c800ebb6b77ca40685b4241360c3
Moritz Tiedje <moritztiedje@gmail.com>
2019-11-03 12:28:21 +0100

    Return performs auto-login

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -7,10 +7,16 @@ import { ClientsProvider } from '../rest-client/rest-client.module';
     styleUrls: ['./login-screen.component.scss'],
 })
 export class LoginScreenComponent {
+
     username: string;
     password: string;
 
     constructor(private clientsProvider: ClientsProvider) {
+        let RETURN_KEY_CODE = 13;
+        document.addEventListener('keyup', (keyEvent: KeyboardEvent) => {
+            if (keyEvent.keyCode == RETURN_KEY_CODE)
+                this.login();
+        })
     }
 
     onUsernameChange(username: string) {
